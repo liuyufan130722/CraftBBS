a82700fe48c95e8c9ef2e21d92a101f324f4fb47
PoppyPlaytime666 <mcxmmm3588981570@163.com>
2024-04-11 12:06:21 +0800

    add vue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-Vue.use(VueRouter);
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from './App.vue'
 
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
